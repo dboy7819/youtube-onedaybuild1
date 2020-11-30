9e3dfb79c41bdb00735bb90a6536d19636dd68f6
James Hayes <jameshayes@Jonathans-iMac.local>
2020-11-29 20:19:07 -0600

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

---
+++ youtube-onedaybuild1/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild1
+//
+//  Created by James Hayes on 11/29/20.
+//
+
+
+import Foundation
+
+struct Constants {
+    
+    static var API_KEY = "[AIzaSyDWSrRWIFadL2FNdsvCHoOW3-xA4Pzeo7A]"
+    static var PLAYLIST_ID = "UUTyEKPXJjmtpYentN9gen7Q"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
---
+++ youtube-onedaybuild1/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  youtube-onedaybuild1
+//
+//  Created by James Hayes on 11/29/20.
+//
+
+
+import Foundation
+
+struct Video {
+    
+    var videoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
